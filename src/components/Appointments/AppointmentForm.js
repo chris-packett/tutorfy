77c6d4dbd6c71182fa8d6dd50b88587fb1d44d8a
Chris <chrispackett1998@gmail.com>
2018-09-24 14:55:02 -0400

    added formatAppointmentLength func & finished dynamic POST request body

--- src/components/Appointments/AppointmentForm.js
+++ src/components/Appointments/AppointmentForm.js
@@ -5,7 +5,10 @@ class AppointmentForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            location: ''          
+            date: new Date(),
+            startTime: "00:00",
+            appointmentLength: 0,
+            location: ''         
         }
     }
 
@@ -19,6 +22,7 @@ class AppointmentForm extends Component {
         e.preventDefault()
         let appointmentData = {
             "StartTime": moment(`${this.state.date}T${this.state.startTime}`, moment.HTML5_FMT.DATETIME_LOCAL).format(),
+            "AppointmentLength": this.state.appointmentLength,
             "Location": this.state.location,
         }
         fetch('https://localhost:5001/api/appointments/add', {
@@ -33,23 +37,45 @@ class AppointmentForm extends Component {
             console.log(appointmentData)
         })
     }
+
+    formatAppointmentLength = () => {
+        if (this.state.appointmentLength == 1) {
+            return `${this.state.appointmentLength} hour`
+        }
+        else if (this.state.appointmentLength % 1 == 0) {
+            return `${this.state.appointmentLength} hours`
+        }
+        else {
+            let total = this.state.appointmentLength
+            let hours = Math.floor(total)
+            let minutes = (total - hours) * 60
+            return `${hours} hours and ${minutes} minutes`
+        }
+    }
     
     render() {
         return (
             <div className="appointment-form-container">
                 <form className="appointment-form" onSubmit={this.handleSubmit}>
                     <label htmlFor="date">Date:</label>
-                    <input type="date" name="date" onChange={this.handleChange}/>
+                    <input type="date" 
+                        name="date" onChange={this.handleChange}
+                    />
 
                     <label htmlFor="startTime">Start Time:</label>
-                    <input type="time" name="startTime" onChange={this.handleChange}/>
+                    <input type="time" 
+                        name="startTime" onChange={this.handleChange}
+                    />
 
-                    {/* <label htmlFor="session-length">How long?</label>
-                    <input type="range" min="0" max="5" value="0" step=".5" 
-                        id="session-length" name="session-length" required/> */}
+                    <label htmlFor="appointmentLength">How long: <span>{this.formatAppointmentLength()}</span></label>
+                    <input type="range" min="0" max="5" step=".5" value={this.state.appointmentLength}  
+                        name="appointmentLength" onChange={this.handleChange}
+                    />
 
                     <label htmlFor="location">Location:</label>
-                    <input type="text" name="location" onChange={this.handleChange}/>
+                    <input type="text" 
+                        name="location" onChange={this.handleChange}
+                    />
 
                     <button className="schedule-appointment-button">Schedule</button>
                 </form>
